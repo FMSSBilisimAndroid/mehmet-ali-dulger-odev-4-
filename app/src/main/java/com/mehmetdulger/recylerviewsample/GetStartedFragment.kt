f3d9b12d35bea8485fb1ca3ddde5225d392236ed
mehmetdulger <mehmetdulger1@gmail.com>
2022-09-18 22:24:44 +0300

    Initial Commit

--- app/src/main/java/com/mehmetdulger/recylerviewsample/GetStartedFragment.kt
+++ app/src/main/java/com/mehmetdulger/recylerviewsample/GetStartedFragment.kt
@@ -1,12 +1,17 @@
 package com.mehmetdulger.recylerviewsample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.TextAppearanceSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mehmetdulger.recylerviewsample.databinding.FragmentGetStartedBinding
+
 
 class GetStartedFragment : Fragment() {
 
@@ -14,7 +19,6 @@ class GetStartedFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -27,10 +31,33 @@ class GetStartedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragmentGetStartedBinding.getStartedButton.setOnClickListener{
-            findNavController().navigate(R.id.action_getStartedFragment_to_catalogFragment)
+
+
+        fragmentGetStartedBinding.apply {
+            getStartedButton.setOnClickListener {
+                findNavController().navigate(R.id.action_getStartedFragment_to_catalogFragment)
+            }
+            titleTextView.text = getString(R.string.lets_explore)
+            setTextStyleSpan(titleTextView, 18, 30, R.style.title_black_style)
+            setTextStyleSpan(titleTextView, 17, 30, R.style.title_orange_style)
+            setTextStyleSpan(titleTextView, 36, 44, R.style.title_orange_style)
         }
 
+    }
 
+    private fun setTextStyleSpan(
+        textView: TextView,
+        startPosition: Int,
+        endPosition: Int,
+        style: Int
+    ) {
+        val spannableStr = SpannableString(textView.text)
+        spannableStr.setSpan(
+            TextAppearanceSpan(context, style),
+            startPosition,
+            endPosition,
+            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        textView.text = spannableStr
     }
 }
